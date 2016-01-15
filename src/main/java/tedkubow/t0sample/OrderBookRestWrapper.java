5abd56ee529b04088c7ff294e68ad869d1495345
tedk <tedk@TEDK-PC4.saccap.int>
2016-01-15 12:00:55 -0500

    add standalone http server and rest support

--- src/main/java/tedkubow/t0sample/OrderBookRestWrapper.java
+++ src/main/java/tedkubow/t0sample/OrderBookRestWrapper.java
@@ -1,28 +1,69 @@
 package tedkubow.t0sample;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+@Path("/test")
 public class OrderBookRestWrapper {
-	
+
 	private OrderBook orderBook;
-	
+
 	public OrderBookRestWrapper() {
-		orderBook = OrderBook.getOrderBook();		
+		orderBook = OrderBook.getOrderBook();
 	}
-	
-	
+
 	public static void main(String[] args) {
-		OrderBookRestWrapper restWrapper = new OrderBookRestWrapper();				
+
+		try {
+			createHttpServer();
+		} catch (IOException e) {
+			throw new IllegalStateException("Can't start http server", e);
+		}
+		OrderBookRestWrapper restWrapper = new OrderBookRestWrapper();
+		System.out.println("running...");
+
 	}
-	
-	
+
+	@POST
 	public void buy(double limitPrice, int quantity) {
 		orderBook.buy(limitPrice, quantity);
 	}
-	
+
+	@POST
 	public void sell(double limitPrice, int quantity) {
 		orderBook.sell(limitPrice, quantity);
 	}
-	
-	
-	
+
+	@GET
+	public List<String> book() {
+		ArrayList<String> l = new ArrayList<String>();
+		l.add("ted test");
+		return l;
+	}
+
+	// REST server specific details
+	private static HttpServer createHttpServer() throws IOException {
+		HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
+		server.createContext("/test", new HttpHandler() {
+
+			public void handle(HttpExchange arg0) throws IOException {
+				System.out.println("got httpexchange..." + arg0);
+
+			}
+		});
+		server.setExecutor(null); // creates a default executor
+		server.start();
+		return server;
+	}
 
 }
