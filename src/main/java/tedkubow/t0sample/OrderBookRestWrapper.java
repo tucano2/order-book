ac871ac4c3b7ade931c8099c3c997219f6c850ae
tedk <tedk@TEDK-PC4.saccap.int>
2016-01-15 12:18:09 -0500

    switch rest wrapper to spark

--- src/main/java/tedkubow/t0sample/OrderBookRestWrapper.java
+++ src/main/java/tedkubow/t0sample/OrderBookRestWrapper.java
@@ -1,69 +1,38 @@
 package tedkubow.t0sample;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import static spark.Spark.get;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-@Path("/test")
 public class OrderBookRestWrapper {
 
 	private OrderBook orderBook;
 
 	public OrderBookRestWrapper() {
 		orderBook = OrderBook.getOrderBook();
+
+		get("/hello", (req, res) -> "Hello World");
 	}
 
-	public static void main(String[] args) {
-
-		try {
-			createHttpServer();
-		} catch (IOException e) {
-			throw new IllegalStateException("Can't start http server", e);
-		}
-		OrderBookRestWrapper restWrapper = new OrderBookRestWrapper();
-		System.out.println("running...");
-
-	}
-
-	@POST
 	public void buy(double limitPrice, int quantity) {
 		orderBook.buy(limitPrice, quantity);
 	}
 
-	@POST
 	public void sell(double limitPrice, int quantity) {
 		orderBook.sell(limitPrice, quantity);
 	}
 
-	@GET
 	public List<String> book() {
 		ArrayList<String> l = new ArrayList<String>();
 		l.add("ted test");
 		return l;
 	}
 
-	// REST server specific details
-	private static HttpServer createHttpServer() throws IOException {
-		HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
-		server.createContext("/test", new HttpHandler() {
+	public static void main(String[] args) {
 
-			public void handle(HttpExchange arg0) throws IOException {
-				System.out.println("got httpexchange..." + arg0);
+		OrderBookRestWrapper restWrapper = new OrderBookRestWrapper();
+		System.out.println("running...");
 
-			}
-		});
-		server.setExecutor(null); // creates a default executor
-		server.start();
-		return server;
 	}
-
 }
